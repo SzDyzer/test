6ae97ca0f53df6e16ccfec243805e94615cea291
SzDyzer <lamy.valentin76770@gmail.com>
2020-11-09 10:37:37 +0100

    git init 2

--- index.php
+++ index.php
@@ -7,3 +7,12 @@
 // git init (une fois au debut)
 
 // git status
+
+//
+//  git config --global user.email "quidelantoine@gmail.com"
+//
+//  git config --global user.name "quidelantoine"
+//
+//
+//
+// git commit -m "init git"
